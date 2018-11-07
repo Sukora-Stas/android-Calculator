d2053409e2e559f57042e44ba0b1df5b3534cedc
SukoraStas <st.sukora@gmail.com>
2018-11-08 00:21:44 +0300

    add operation

--- app/src/main/java/com/androiddev/bsuir/android_calculator/MainActivity.java
+++ app/src/main/java/com/androiddev/bsuir/android_calculator/MainActivity.java
@@ -1,13 +1,42 @@
 package com.androiddev.bsuir.android_calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView textResult;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+    private void initView() {
+        textResult = (TextView) findViewById(R.id.textResult);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+
+    // обработка нажатия на числовую кнопку
+    public void onNumberClick(View view) {
+
+        Button button = (Button) view;
+        textResult.append(button.getText());
+
+    }
+
+    public void onOperationClick(View view){
+
+
+    }
+
 }
